0ecf3139dcc2a7eac4939d9485baa65251e54f81
Vivi55100 <steven.halliez@hotmail.fr>
2024-04-12 15:58:05 +0200

    Try push

---
+++ view/base.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap-utilities.min.css">
+    <script defer src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script defer src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script defer src="../js/main.js"></script>
+    <title>NavBar</title>
+</head>
+<body>
+  
+</body>
+</html>
