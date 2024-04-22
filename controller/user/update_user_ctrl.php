7b28f4f6c77c3e92a3ae108bf2da3fed1c7d7002
Vivi55100 <steven.halliez@hotmail.fr>
2024-04-22 15:57:42 +0200

    try fix remove last avatar

--- controller/user/update_user_ctrl.php
+++ controller/user/update_user_ctrl.php
@@ -7,21 +7,21 @@ if(!empty($_POST["last_name"]) && !empty($_POST["first_name"]) && !empty($_POST[
 
      // Vérification de la taille et du type du fichier
      if ($_FILES['avatar']['size'] > 0 && $_FILES['avatar']['error'] === UPLOAD_ERR_OK) {
-        /* $_FILES['avatar']['size'] > 0 : Vérifie si la taille du fichier téléchargé est supérieure à zéro.
-            $_FILES['avatar']['error'] === UPLOAD_ERR_OK : Vérifie s'il n'y a pas d'erreur lors du téléchargement du fichier.
-        */
+        // $_FILES['avatar']['size'] > 0 : Vérifie si la taille du fichier téléchargé est supérieure à zéro.
+        // $_FILES['avatar']['error'] === UPLOAD_ERR_OK : Vérifie s'il n'y a pas d'erreur lors du téléchargement du fichier.
+        
 
         $file_tmp = $_FILES['avatar']['tmp_name'];
         // $file_tmp = $_FILES['avatar']['tmp_name'] : Récupère le chemin temporaire du fichier téléchargé.
-        // var_dump("chemin temporaire ▼", $file_tmp);
+        var_dump("$ file_tmp chemin temporaire ▼", $file_tmp);
 
         $file_name = basename($_FILES['avatar']['name']);
         // $file_name = basename($_FILES['avatar']['name']) : Récupère le nom du fichier téléchargé.
-        // var_dump("nom du fichier ▼", $file_name);
+        var_dump("$ file_name nom du fichier ▼", $file_name);
 
         $upload_dir = UPLOAD_DIR;
         // $upload_dir = "assets/img/upload/" : Définit le répertoire de destination où le fichier sera déplacé après le téléchargement.
-        // var_dump("destination où le fichier sera déplacé ▼", $upload_dir);
+        var_dump("$ upload_dir destination où le fichier sera déplacé ▼", $upload_dir);
 
         // Déplacement du fichier téléchargé vers le dossier de destination
         if (move_uploaded_file($file_tmp, $upload_dir . $file_name)) {
@@ -29,33 +29,37 @@ if(!empty($_POST["last_name"]) && !empty($_POST["first_name"]) && !empty($_POST[
 
             $avatar_path = "assets/img/upload/" . $file_name;
             // $avatar_path = $upload_dir . $file_name : Concatène le chemin du répertoire de destination avec le nom du fichier pour obtenir le chemin complet de l'avatar.
-            // var_dump("chemin du répertoire de destination avec le nom du fichier ▼", $avatar_path);
+            var_dump("$ avatar_path : chemin du répertoire de destination avec le nom du fichier ▼", $avatar_path);
 
             // Récupérer la liste des fichiers dans le dossier "upload"
             $files = scandir($upload_dir);
+            var_dump("$ files ▼", $files);
 
             // Supprimer uniquement l'avatar précédent
-            if(isset($_SESSION['previous_avatar'])) {
-                unlink($_SESSION['previous_avatar']);
-                unset($_SESSION['previous_avatar']);
+            if(isset($_SESSION['previous_avatar'][$_POST['id_user']])) {
+                unlink($_SESSION['previous_avatar'][$_POST['id_user']]);
+                unset($_SESSION['previous_avatar'][$_POST['id_user']]);
             }
 
             // Stocker le chemin de l'avatar actuel pour la prochaine fois
-            $_SESSION['previous_avatar'] = $upload_dir . $file_name;
+            $_SESSION['previous_avatar'][$_POST['id_user']] = $upload_dir . $file_name;
+            var_dump("session previous avatar ▼", $_SESSION['previous_avatar']);
+            var_dump("session user id ▼", $_POST['id_user']);
+            var_dump("$ _SESSION['previous_avatar'][$ _POST['id_user']] ▼", $_SESSION['previous_avatar'][$_POST['id_user']]);
 
             // Parcourir la liste des fichiers
-            foreach ($files as $file) {
-                // Vérifier si le fichier est un fichier d'image et s'il n'est pas l'avatar actuel
-                if (in_array(pathinfo($file, PATHINFO_EXTENSION), ['jpg', 'jpeg', 'png']) && $file !== $file_name) {
-                    // Supprimer le fichier inutile
-                    unlink($upload_dir . $file);
-                }
-            }
+            // foreach ($files as $file) {
+            //     // Vérifier si le fichier est un fichier d'image et s'il n'est pas l'avatar actuel
+            //     if (in_array(pathinfo($file, PATHINFO_EXTENSION), ['jpg', 'jpeg', 'png']) && $file !== $file_name) {
+            //         // Supprimer le fichier inutile
+            //         unlink($avatar_path . $file);
+            //     }
+            // }
         } else {
             echo "<h2 class='text-center'>Erreur lors du téléchargement de l'avatar.</h2>"; // Affiche un message d'erreur si le téléchargement du fichier a échoué.
             exit;
         }
-     } else {
+    } else {
         echo "<h2 class='text-center'>Veuillez sélectionner un fichier d'avatar valide.</h2>"; // Affiche un message d'erreur si aucun fichier n'a été sélectionné ou si le fichier est invalide.
         exit;
     }
@@ -74,7 +78,7 @@ if(!empty($_POST["last_name"]) && !empty($_POST["first_name"]) && !empty($_POST[
         // Réattribuer la session pour "actualiser" la page et ainsi faire apparaitre l'avatar sans devoir quitter la session
         $_SESSION["avatar"] = $userReset["avatar"];
         echo "<h2 class='text-center'>La modification est reussie</h2>";
-        header("Location:../../view/home/home.php");
+        //header("Location:../../view/home/home.php");
     }catch(PDOException $e){
         echo "<h2 class='text-center'>La modification a échouée !</h2>" . $e->getMessage();
     }
