734800a759afcec0f4c574d1406899a68c5e2413
Vivi55100 <steven.halliez@hotmail.fr>
2024-04-24 13:41:24 +0200

    recuperation data $_FILES

--- controller/user/update_avatar_ctrl.php
+++ controller/user/update_avatar_ctrl.php
@@ -6,7 +6,7 @@ include_once "../../model/pdo.php";
 /*
     Recuperer l'addresse de l'ancienne image pour pouvoir la supprimer ✅
 
-    Recuperer les données du $_FILES
+    Recuperer les données du $_FILES ✅
     
     Recuperer l'extension (jpg , png ...)
 
@@ -19,11 +19,29 @@ include_once "../../model/pdo.php";
     Renommer le fichier en hash (sha, md5)
 
     Inserer dans la base de donnée
-    (si tout c'est bien passer if(blabla){unset l'ancien image})
+    (si tout c'est bien passer if(blabla){unset l'ancienne image})
 */
 
-$oldAvatar = $_SESSION['avatar'];
-var_dump("old avatar = ", $oldAvatar, "<br>");
+$userSession = $_SESSION;
+var_dump("$ user session : ", $userSession, "<br><br>");
 
-$dataFile = $_FILES['avatar'];
-var_dump("data file = ", $dataFile, "<br>");
+$oldAvatar = $_SESSION['avatar'];
+var_dump("old avatar = ", $oldAvatar, "<br><br>");
+
+$avatarDataFile = $_FILES['avatar'];
+var_dump("avatar data file = ", $avatarDataFile, "<br><br>");
+
+$avatarName = $_FILES['avatar']['name'];
+var_dump('$ avatar name =  files name : ', $avatarName, "<br><br>");
+
+$avatarFullPath = $_FILES['avatar']['full_path'];
+var_dump('$ avatar full path : ', $avatarFullPath, "<br><br>");
+
+$avatarTmpName = $_FILES['avatar']['tmp_name'];
+var_dump('$ avatar tmp Name : ', $avatarTmpName, "<br><br>");
+
+$avatarSize = $_FILES['avatar']['size'];
+var_dump('$ avatar size: ', $avatarSize, "<br><br>");
+
+$avatarError = $_FILES['avatar']['error'];
+var_dump('$ avatar error  : ', $avatarError, "<br><br>");
