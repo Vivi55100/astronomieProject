ceaf5f8fe5cde5a73ad1c900f81615fe446718bf
Vivi55100 <steven.halliez@hotmail.fr>
2024-04-26 12:27:00 +0200

    question lvl 1 ok

--- view/admin/indexQuestion.php
+++ view/admin/indexQuestion.php
@@ -1,9 +1,38 @@
 <?php
     include_once "../base.php";
     include_once "../../model/role.php";
+    include_once "../../model/pdo.php";
     
     if($_SESSION["role"] && $_SESSION["role"] == Role::ADMIN->value){
 ?>
+
+<table class="table table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Question content</th>
+                <th>Id_difficulty</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                $table = "";
+                $sql = "SELECT * FROM question";
+                $stmt = $pdo->query($sql);
+                $questions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                if($questions){
+                    foreach($questions as $question){
+                        $table .= "<tr>";
+                            $table .= "<td>" . htmlentities($question['id_question']) . "</td>";
+                            $table .= "<td>" . htmlentities($question['question_content']) . "</td>";
+                            $table .= "<td>" . htmlentities($question['id_difficulty']) ."</td>";
+                        $table .="</tr>";
+                    }
+                    echo $table;
+                }
+            ?>
+        </tbody>
+    </table>
 
 <?php
     }else{
