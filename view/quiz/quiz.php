d9796b9bc1e617852f17d72eac8891d2824a126f
Vivi55100 <steven.halliez@hotmail.fr>
2024-05-02 15:31:31 +0200

    steel try fix quiz shuffle

--- view/quiz/quiz.php
+++ view/quiz/quiz.php
@@ -3,6 +3,16 @@
     include_once "../base.php";
     if ($_SESSION['role'] >= Role::LOGGED->value){
         $id_quiz = $_GET['id_quiz'];
+
+        function array_shuff(&$array) {
+            $keys = array_keys($array);
+            shuffle($keys);
+            foreach ($keys as $key) {
+                $new[$key] = $array[$key];
+            }
+            $array = $new;
+            return true;
+        }
 ?>
 
 <a class="btn btn-primary my-2" href="view/quiz/indexQuiz.php">Retour index quiz</a>
@@ -15,8 +25,8 @@
         // requete de gros batard
 
             "SELECT quiz.id_quiz, quiz.quiz_name, question.id_question, question.question_content, GROUP_CONCAT(response.response_content SEPARATOR '_')
-            AS responses, GROUP_CONCAT(response.is_correct SEPARATOR '_')
-            AS isCorrect
+            AS answers_name, GROUP_CONCAT(response.is_correct SEPARATOR '_')
+            AS answers_value
             FROM quiz
             INNER JOIN question
             ON quiz.id_quiz = question.id_quiz
@@ -25,43 +35,63 @@
             WHERE quiz.id_quiz = $id_quiz
             GROUP BY question.id_question";
 
-
             $quizQuestionStmt = $pdo->query($quizQuestionSql);
-
             $quizQuestions = $quizQuestionStmt->fetchAll(PDO::FETCH_ASSOC);
             $newQuizQuestions = [];
 
             // Apres la requete ci-dessus, je souhaite recuperer afficher aleatoirement une question a la fois
             // $randQuestion = rand(0, count($quizQuestions) - 1);
             // On va traiter les données qui arrivent en string à cause de la concatenation
+
             foreach ($quizQuestions as $qq) {
+
                 $tempArray = [];
                 $tempArray['id_quiz'] = $qq['id_quiz'];
                 $tempArray['quiz_name'] = $qq['quiz_name'];
-                // rajouter id_question ainsi que les explode de responses et isCorrect ne pas oublier d'importer la requete sql dans phpmyadmin
-                $qqR = [ "responses" => explode('_', $qq['responses']), "isCorrect" =>  explode('_', $qq['isCorrect'])];
-                array_replace($quizQuestions,  $qqR);
+                $tempArray['id_question'] = $qq['id_question'];
+                $tempArray['question_content'] = $qq['question_content'];
+                $tempArrayName = explode('_', $qq['answers_name']);
+                $tempArrayValue = explode('_', $qq['answers_value']);     
+
+                    // var_dump($name);
+                    foreach($tempArrayName as $index => $name){
+                        // var_dump($value);
+                        $tempArray["answers"][$name] = $tempArrayValue[$index];
+                    }
+                
+                $newQuizQuestions[] = $tempArray;
+                $tempArray = [];
             }
+
+            foreach ($newQuizQuestions as $question) {
+                $q = $question["answers"];
+                unset($question["answers"]);
+                array_shuff($q);
+                $question["answers"] = $q;
+            }
+            //array_shuff($newQuizQuestions[0]["answers"]);
+            // $newQuizQuestions[0]["answers"] = $p;
+            //shuffle($newQuizQuestions);
+            var_dump($newQuizQuestions);
         ?>
         <h4 id="quizQuestionContent">
-            <?= var_dump($quizQuestions[0]) ?>;
+            <?php
+                // var_dump($newQuizQuestions[0]);
+                // foreach ($newQuizQuestions as $question) {
+                //     echo "<p>$question[question_content]</p>";
+                // }
+            ?>
+
         </h4>
     </div>
-    <div class="quizResponse d-flex">
-        <!-- < ?php
-            $quizResponseSql = "SELECT * FROM response WHERE id_question";
-            $quizResponseStmt = $pdo->query($quizResponseSql);
-            $quizResponses = $quizResponseStmt->fetchAll(PDO::FETCH_ASSOC);
-        ?> -->
-    </div>
+    <div class="quizResponse d-flex"></div>
 </div>
 
 
 
 <script>
-    const quizQuestionContent = document.getElementById('quizQuestionContent')
+     document.body.style.overflowY = 'scroll';
 </script>
 </body>
 </html>
 <?php } ?>
-<!-- $sql = "SELECT * FROM quiz INNER JOIN question ON quiz.id_quiz = question.id_quiz WHERE quiz.id_quiz = 1"; -->
