315fa466b4662a605b4ef39cbed01a3cec2d85d7
Vivi55100 <steven.halliez@hotmail.fr>
2024-05-09 13:07:56 +0200

    CGU ok responsive ok

--- view/CGU/cguPage.php
+++ view/CGU/cguPage.php
@@ -6,22 +6,21 @@
 
 <div class="cguPage">
 
-    <h1 class="text-center my-3">Contact</h1>
+    <h1 class="text-center my-3">Conditions Générales d'Utilisation</h1>
 
     <div class="CGU">
 
         <div>
-            <h5>Conditions Générales d'Utilisation</h5>
-
             <p>
                 Bienvenue sur mon projet, un site dédié à fournir des informations sur l'astronomie.
-                En utilisant ce site, vous acceptez les conditions énoncées ci-dessous. Veuillez les lire attentivement avant d'accéder ou d'utiliser le site.
+                En utilisant ce site, vous acceptez les conditions énoncées ci-dessous. Veuillez les lire attentivement.
             </p>
 
             <h6>Introduction</h6>
             <p>
                 Mon projet a pour objectif de fournir des informations informatives et éducatives sur l'astronomie.
                 Le contenu comprend pour le moment, un quiz contenant 3 niveaux de difficultés.
+                De nombreux axes d'améliorations sont en cours de développement.
             </p>
 
             <h6>Acceptation des CGU</h6>
@@ -32,49 +31,52 @@
 
             <h6>Propriété intellectuelle</h6>
             <p>
-                Tous les contenus présents sur mon projet, y compris mais sans s'y limiter, les articles, les images et les vidéos, sont la propriété intellectuelle de [Nom de l'entreprise] ou de ses concédants de licence.
-                Vous pouvez utiliser ce contenu à des fins personnelles et non commerciales, mais toute reproduction ou distribution sans autorisation est strictement interdite.
+                Les images proviennent de plusieurs sources, telles que : 
+                <span><a href="https://images.nasa.gov/">L'Agence Spatiale Américaine (NASA)</a></span> ,
+                <span><a href="https://www.esa.int/ESA_Multimedia/Images">L'agence Spatiale Européenne (ESA)</a></span>.<br>
+                Vous pouvez utiliser ce contenu à des fins personnelles et non-commerciales, mais toute reproduction ou distribution sans autorisation est strictement interdite.
             </p>
+
             <h6>Responsabilités de l'utilisateur</h6>
             <p>
                 Les informations fournies sur mon projet sont à titre informatif seulement.
-                Nous nous efforçons de garantir l'exactitude et la fiabilité de ces informations,
-                mais nous ne pouvons pas garantir leur exhaustivité ou leur pertinence pour des cas particuliers.
+                Je m'efforce de garantir l'exactitude et la fiabilité de ces informations,
+                mais je ne peux pas garantir leur exhaustivité ou leur pertinence pour des cas particuliers.
                 L'utilisateur est seul responsable de l'interprétation et de l'utilisation des informations fournies.
             </p>
 
             <h6>Liens externes</h6>
             <p>
-                Notre site peut contenir des liens vers d'autres sites web qui ne sont pas sous notre contrôle.
-                Nous n'endossons pas le contenu de ces sites tiers et nous déclinons toute responsabilité quant à leur contenu ou à leurs pratiques de confidentialité.
+                Mon site peut contenir des liens vers d'autres sites web qui ne sont pas sous mon contrôle.
+                Je n'endosse pas le contenu de ces sites tiers et je décline toute responsabilité quant à leur contenu ou à leurs pratiques de confidentialité.
             </p>
 
             <h6>Protection des données personnelles</h6>
             <p>
-                Nous respectons la confidentialité de nos utilisateurs. Les informations personnelles collectées sur ce site sont utilisées uniquement dans le but de fournir et d'améliorer nos services.
-                Nous ne vendons ni ne louons vos informations à des tiers.
+                Je respecte la confidentialité de mes utilisateurs.
+                Les informations personnelles collectées sur ce site sont utilisées uniquement dans le but de fournir et d'améliorer mes services.
+                Je ne vends ni ne loue vos informations à des tiers.
             </p>
 
             <h6>Modification des CGU</h6>
             <p>
-                Nous nous réservons le droit de modifier ces Conditions Générales d'Utilisation à tout moment.
+                Je me réserve le droit de modifier ces Conditions Générales d'Utilisation à tout moment.
                 Les modifications seront effectives dès leur publication sur le site.
                 Il est de votre responsabilité de consulter régulièrement ces conditions pour être informé des changements.
             </p>
 
             <h6>Droit applicable et juridiction compétente</h6>
-            <p>Ces Conditions Générales d'Utilisation sont régies par les lois Française</p>
+            <p>Ces Conditions Générales d'Utilisation sont régies par les lois françaises.</p>
 
             <h6>Contact</h6>
             <p>
                 Si vous avez des questions ou des préoccupations concernant ces Conditions Générales d'Utilisation ou le fonctionnement de mon projet,
-                veuillez nous contacter à <span><i>steven.halliez@gmail.com</i></span>.
-                
+                veuillez nous contacter à <span><i style="font-size: 1.2em; font-weight: bolder; text-decoration: underline;">steven.halliez@gmail.com</i></span>.
             </p>
 
             <p>En utilisant mon projet, vous acceptez ces Conditions Générales d'Utilisation.</p>
 
-            <h5>
+            <h5 style="text-decoration: underline;">
                 Merci de votre visite et de votre intérêt pour l'astronomie ! 
             </h5>
         </div>
