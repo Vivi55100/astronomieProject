2486f677520e6fa3cf15ce87c000c1d6fce9d237
Vivi55100 <steven.halliez@hotmail.fr>
2024-05-09 15:14:40 +0200

    responsive indexQuiz, quiz

--- view/quiz/quiz.php
+++ view/quiz/quiz.php
@@ -3,11 +3,11 @@
     include_once "../../model/role.php";
     if ($_SESSION['role'] >= Role::LOGGED->value){
 ?>
-    <a class="btn btn-primary my-2 mx-3" href="view/quiz/indexQuiz.php">
+    <a class="btn btn-primary my-2 mx-3" href="view/quiz/indexQuiz.php"> <!-- Redirection link transformed into a button -->
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-left" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5"/>
         </svg>
-    </a> <!-- Redirection link transformed into a button -->
+    </a> 
 
     <div class="quizGame container d-flex flex-column" id="quizGame">
 
@@ -32,7 +32,7 @@
     let response = []
     let score = 0
     let idQuiz = urlParams.get("id_quiz")
-    console.log("idQuiz = ", idQuiz)
+    //console.log("idQuiz = ", idQuiz)
 
 
     document.addEventListener("DOMContentLoaded", function (){
@@ -47,15 +47,15 @@
         fetch("../../controller/admin/quiz/quiz_ctrl.php", data)
         .then(response => response.json())
         .then(quiz => {
-            console.log("quiz = ", quiz)
-            questions = [...quiz] // Avec le spread operator on copie le tableau de la base de donnée dans un autre tableau ( clonage )
+            //console.log("quiz = ", quiz)
+            questions = [...quiz] // With the spread operator, we copy the array from the database into another array (cloning)
             // console.log(" question[0] : ", questions[0].question_content)
 
             questionMaker(questions)
         })
     })
 
-    function questionMaker(questions) { // Fonction qui crée un tableau de question ...
+    function questionMaker(questions) {
         //console.log("questions : ", questions)
         if(questions.length > 0){
             nextQuestion.innerHTML = ""
@@ -65,14 +65,14 @@
             let showResponses = "<form id='formValidation'>"
             for (const res in questions[0].answers) {
                 //console.log(`response : ${res} => value : ${questions[0].answers[res]}`)
-                showResponses += `<input class='response col-2 text-center ' type='radio' name='response'  value='${questions[0].answers[res]}'><label class='my-2 col-10 text-center' for='response'>${res}</label>` // Pour éviter de tricher nous mettons ça a l'interieur d'une variable dans l'élément input
+                showResponses += `<input class='response col-2 text-center ' type='radio' name='response'  value='${questions[0].answers[res]}'><label class='my-2 col-10 text-center' for='response'>${res}</label>` // To prevent cheating, we encapsulate it within a variable inside the input element
             }
             showResponses += `<div class="text-center mt-3"><input type="submit" class="btn btn-info w-50" id="btnValidation" value="Valider"></div>`
             showResponses += "</form>"
 
             quizResponse.innerHTML = showResponses
 
-            const form = document.getElementById('formValidation') // On recupere le formulaire ici car avant le formulaire n'existait pas dans le DOM ( promise le crée à l'instant )
+            const form = document.getElementById('formValidation') // We retrieve the form here because before, the form did not exist in the DOM (the promise creates it instantly)
             const btnSubmit = document.getElementById('btnValidation')
 
             form.addEventListener("submit", function(e){
@@ -89,15 +89,15 @@
                     //console.log("checkData = ", checkData)
                     const responseInputs = document.querySelectorAll(".response")
                     if(checkData){
-                        console.log("checkData = ", checkData)
-                        score++ // Ici on augmente le score de 1
-                        // Ici on a bien repondu, la bonne reponse se colore en vert, les mauvaises en rouge.
+                        //console.log("checkData = ", checkData)
+                        score++ // Here, we increase the score by 1
+                        // Here, we have answered correctly. The correct answer is highlighted in green, while incorrect ones are highlighted in red
                         
                         //console.log("response inputs : ", responseInputs)
                         responseInputs.forEach(input => {
                             //console.log('input next . ', input.nextSibling)
                             if(input.checked){
-                                input.nextSibling.style.color = "chartreuse" // nextSibling permet de recuperer l'element suivant voisin ( donc le label dans ce cas ) pour pouvoir le colorié
+                                input.nextSibling.style.color = "chartreuse" // nextSibling allows us to retrieve the next adjacent element (in this case, the label) so that we can color it
                                 input.nextSibling.innerHTML += ' - Bonne réponse'
                             }else{
                                 input.nextSibling.style.color = "red"
@@ -124,7 +124,6 @@
                             responseInputs.forEach(input => {
                             //console.log('input next . ', input.nextSibling)
                                 if(input.value == goodId){
-                                    input.nextSibling.style.color = "chartreuse" // nextSibling permet de recuperer l'element suivant voisin ( donc le label dans ce cas ) pour pouvoir le colorié
                                     input.nextSibling.innerHTML += ' - Bonne réponse'
                                 }else{
                                     input.nextSibling.style.color = "red"
@@ -135,21 +134,19 @@
                     }
 
                     btnSubmit.remove()
-                    console.log("score = ", score)
+                    //console.log("score = ", score)
                     nextQuestion.innerHTML = "<button class='btn btn-primary' id='btnNextQuestion'>Question suivante</button>"
 
                     const btnNextQuestion = document.getElementById('btnNextQuestion')
 
                     btnNextQuestion.addEventListener('click', function() {
-                        questions.shift() // Supprime le 1er element du tableau et décalle les index ( ex: index 1 => index 0 )
-                        questionMaker(questions) // Rappel de la fonction questionMaker, qui devient une fonction recursive ( une fonction qui s'appelle elle-même )
+                        questions.shift() // Remove the first element of the array and shift the indexes accordingly (ex: index 1 becomes index 0)
+                        questionMaker(questions) // Reminder of the function questionMaker, which becomes a recursive function (a function that calls itself)
                     })
                 })
             })
         }else{
-            /*
-                Ici on fera du code pour gerer la fin du quiz
-            */
+            // Code for end of quiz
             quizGame.innerHTML = ""
             if(score <= 7){
                 quizGame.innerHTML += "<h2 class='text-center'>Votre score est de " + score + " Vous avez eu un petit score</h2>"
