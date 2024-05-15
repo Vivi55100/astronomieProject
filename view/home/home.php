9f51d7f6b65608e6cad08607c295a5d939619049
Vivi55100 <steven.halliez@hotmail.fr>
2024-05-15 21:04:37 +0200

    refacto home

--- view/home/home.php
+++ view/home/home.php
@@ -1,7 +1,15 @@
 <?php
     include_once "./../base.php";
+    include_once "../../model/role.php";
 ?>
-<div class="homepage"></div>
+<div class="homepage">
+    <?php
+        if(!isset($_SESSION['role'])){
+            echo "<p class='text-center pt-3'>Bonjour et bienvenue sur mon site sur l'Astronomie, pour accéder aux quiz veuillez vous connecter. Merci de votre visite !</p>";
+        }
+    ?>
+    
+</div>
 
 </body>
 </html>
