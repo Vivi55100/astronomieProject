6b04e72c9ad4f51912bc19503e22487edf2679ee
Vivi55100 <steven.halliez@hotmail.fr>
2024-05-15 22:50:49 +0200

    push to prod

--- model/pdo.php
+++ model/pdo.php
@@ -1,5 +1,5 @@
 <?php
 
 //$pdo = new PDO("mysql:host=localhost;dbname=astronomie_db", "stagiaire", "Stagiaire"); // Linux
-$pdo = new PDO("mysql:host=localhost;dbname=astronomie_db", "root", ""); // Windows
-//$pdo = new PDO("mysql:host=localhost:3306;dbname=astronomie_db", "steven", "xa3ML5husYT479"); // VPS
+//$pdo = new PDO("mysql:host=localhost;dbname=astronomie_db", "root", ""); // Windows
+$pdo = new PDO("mysql:host=localhost:3306;dbname=astronomie_db", "steven", "xa3ML5husYT479"); // VPS
